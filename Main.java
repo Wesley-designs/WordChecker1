37fef9c08344d1f3f033b77a2eb79d3381b2df5c
Wesley-designs <jiewesleyd2789@nycstudents.net>
2024-10-21 13:33:24 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        WordChecker f = new WordChecker();
+        f.createList("1,2,3,4,5");
+        System.out.println(f);
+    }
+}   
