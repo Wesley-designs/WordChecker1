2c8dc3042e016cbb294323c95bce8b085527a5bd
Wesley-designs <jiewesleyd2789@nycstudents.net>
2024-10-28 13:32:55 +0000

    e

--- Main.java
+++ Main.java
@@ -3,9 +3,10 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 
-        WordChecker a = new WordChecker("cat,cats,catsofcosmos");
-        System.out.println(a);
+        WordChecker a = new WordChecker("an,band,band,abandon");
         System.out.println(a.isWordChain());
-        System.out.println(a.createList("cat"));
+        WordChecker b = new WordChecker("to,too,stool,tools");
+        System.out.println(b.isWordChain());
+        WordChecker c = new WordChecker("to,too,stool,tools");
     }
 }   
